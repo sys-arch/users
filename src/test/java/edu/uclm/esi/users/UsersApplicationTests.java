c20ce531b207a96e07a12e025760f5a1c03d48f7
‎WM <91469450+sys-arch@users.noreply.github.com>
2025-03-09 19:28:55 +0100

    Inicio Proyecto

---
+++ src/test/java/edu/uclm/esi/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uclm.esi.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
