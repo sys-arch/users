e774fb3847b869840344cea28ec2901424db1913
Luis <luiseduardo.fdez@alu.uclm.es>
2025-03-11 14:17:54 +0100

    H2 y secreto JWT

--- src/main/java/edu/uclm/esi/users/model/Credits.java
+++ src/main/java/edu/uclm/esi/users/model/Credits.java
@@ -1,10 +1,12 @@
 package edu.uclm.esi.users.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class Credits {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
