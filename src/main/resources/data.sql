24aaf0b8600229aa1e85b02d928e90be92d9e20c
‎WM <91469450+sys-arch@users.noreply.github.com>
2025-04-03 09:30:12 +0200

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,8 @@
+/*La contraseña es: juan.delgado*/
 INSERT INTO Usuarios (id, email, pwd, nombre, apellido1, apellido2) 
 VALUES ('c084162a-133f-4046-8866-fe5b8f43f6c9', 'juan.delgado@ejemplo.com', '
-06844fe66b5fd3753dee2ee7c15f79f1e29956a0a2e724bfeb8e7cfb428a1082cb0eb9026948810c406321a4fb732ad68105eb2dc119d5f2c64f414d339f1294
+ff86ec82a5e1bdc881a3cdde8ab87fbf28a4731b59875a2757046d900e062e416e3ef08c34d2775bcefc39f0313927c6d53c3507c2c05d3392a7b7ad2179a4d9
 ', 'Juan', 'Delgado', 'Pérez');
+
+INSERT INTO Credits (id, userId, credits) 
+VALUES (1, 'c084162a-133f-4046-8866-fe5b8f43f6c9', 200);
