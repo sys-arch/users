f35d1c6cb8d34ab655f3e80d303345b2ba248384
‎WM <91469450+sys-arch@users.noreply.github.com>
2025-05-20 18:21:33 +0200

    Act. 20/05

--- src/main/java/edu/uclm/esi/users/http/PasswordResetController.java
+++ src/main/java/edu/uclm/esi/users/http/PasswordResetController.java
@@ -6,10 +6,15 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import edu.uclm.esi.users.services.PasswordResetService;
 import edu.uclm.esi.users.services.EmailService;
+import edu.uclm.esi.users.services.PasswordResetService;
 import edu.uclm.esi.users.services.PasswordService;
 
 @RestController
